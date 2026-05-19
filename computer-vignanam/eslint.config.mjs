9a94296a579e19f005dd208e2b54a5ec3fbd5cd1
Mayuri Kharwade <mayurikharwade10@gmail.com>
2026-05-19 21:34:00 +0530

    fix eslint

--- computer-vignanam/eslint.config.mjs
+++ computer-vignanam/eslint.config.mjs
@@ -1,16 +1,6 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals.js";
+import { defineConfig } from "eslint/config";
+import nextVitals from "eslint-config-next/core-web-vitals";
 
-const eslintConfig = defineConfig([
-  ...nextVitals,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
+export default defineConfig([
+  nextVitals,
 ]);
-
-export default eslintConfig;
