d6d49bff885f69f09e21997eff3f6d50e2f9ce51
Mayuri Kharwade <mayurikharwade10@gmail.com>
2026-05-20 12:10:18 +0530

    fixed next config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,15 +1,31 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "standalone",
+
   images: {
     remotePatterns: [
-      { protocol: "https", hostname: "images.unsplash.com" },
-      { protocol: "https", hostname: "computervignanam.net" },
-      { protocol: "https", hostname: "graphics.computervignanam.net" },
-      { protocol: "https", hostname: "placehold.co" },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+      {
+        protocol: "https",
+        hostname: "computervignanam.net",
+      },
+      {
+        protocol: "https",
+        hostname: "graphics.computervignanam.net",
+      },
+      {
+        protocol: "https",
+        hostname: "placehold.co",
+      },
     ],
+
     deviceSizes: [640, 750, 828, 1080, 1200],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
   },
+
   experimental: {},
 };
 
